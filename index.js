9b7ee8d5614421ef83d8cb63264b515db0d81181
Anirudh <anirudh.dev314159@gmail.com>
2024-12-19 07:24:29 +0530

    fonts, input field and css style has been changed

--- index.js
+++ index.js
@@ -13,23 +13,22 @@ button.onclick = function getTable(){
     const getNum = Number(txt);
 
     for(let i=1; i<=max; i++){
-        if(getNum === 0)
-    {
-         output.innerHTML = "That's not a valid input"
-        
-    }
-        else{
+
+        if(getNum===0)
+        {
+            output.innerHTML = "Error! Invalid input"
+        }
+        else
+        {
             initialValue +=`${getNum} x ${i} = ${getNum * i}<br>`
             output.innerHTML = initialValue;
         }
+        }
 }
-}
-
 
 reset.onclick = function(){
     initialValue = ""
     output.innerHTML = initialValue;
+    document.getElementById("txtNumber").value = 0;
+
 }
-
-
-
